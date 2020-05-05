ed98d3cb667e4fd206665ae4c380fe401d0a9acd
Sumit Bhatia <sumit.bhatia000@gmail.com>
2020-05-05 20:09:15 +0530

    signup spam issue fixed mail

--- resources/views/emails/signupSuccess.blade.php
+++ resources/views/emails/signupSuccess.blade.php
@@ -1,29 +1,6 @@
-@extends('layouts.email')
-@section('content')
 
 <p>Hi <b>{{$model->full_name}}</b>,</p>
-                        <p>
-                           {{__('mails.thank_you_signup',['sitename'=>env('APP_NAME')])}} 
-                           <br/>
-                           {!!__('mails.your_unique_code',['unique_code'=>"<b>{$model->unique_code}</b>"])!!}     
-
-                        </p>
-                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
-                          <tbody>
-                            <tr>
-                              <td align="left">
-                                <table role="presentation" border="0" cellpadding="0" cellspacing="0">
-                                  <tbody>
-                                    <tr>
-                                      
-                                      <td> <a href="{{url('/')}}" target="_blank">{{__('Visit :sitename',['sitename'=>env('APP_NAME')])}}</a> </td>
-                                    </tr>
-                                  </tbody>
-                                </table>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                       
-
-@endsection
+                      
+{{__('mails.thank_you_signup',['sitename'=>env('APP_NAME')])}} 
+<br/>
+{!!__('mails.your_unique_code',['unique_code'=>"<b>{$model->unique_code}</b>"])!!}   </p>
